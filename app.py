83adf42c84862aebaee2383208f1f9c1ad490f26
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-06 22:42:26 +0800

    3rd

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
 import streamlit as st
 import os
-
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCj6LMLXmGUXBr2k2OObddZWkVyPxE2cp8"
-
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
-our_query = "What is the currency of India?"
-result = llm.invoke(our_query)
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCkoQCn0rlZuRaUZioYsuEAy9JFWrfInc0"
 
-prompt = st.chat_input("Say something")
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+prompt = st.chat_input("Say something.")
 if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-    message = st.chat_message("assistant")
-    message.write("Hello human")
+
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    # Generate a response using the Gemini LLM.
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    messages= [
+        {"role": m["role"], "content": m["content"]}
+        for m in st.session_state.messages
+    ]
+    stream = llm.invoke(messages)
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream.content)
+    st.session_state.messages.append({"role": "assistant", "content": response})
